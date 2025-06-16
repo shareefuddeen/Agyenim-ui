2d16ffcefb0d32d88dc83ea267c91a8859617ab8
shareefuddeen <140364218+shareefuddeen@users.noreply.github.com>
2025-06-16 08:15:49 -1200

    Even

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -1,12 +1,12 @@
-import ImageSlider from "./Imageslider";
+import ImageSlider from "./ImageSlider";
 
 const Events = () => {
-
   return (
     <div className="h-[120vh] bg-gray-200 flex-col justify-center items-center p-4">
-      <h1 className="mt-16 text-4xl text-center">Our latest contributions to communities</h1>
+      <h1 className="mt-16 text-4xl text-center">
+        Our latest contributions to communities
+      </h1>
       <div className="w-[100%] mt-[4em]">
-
         <ImageSlider />
       </div>
     </div>
