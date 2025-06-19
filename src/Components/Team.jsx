20a4914d1936c9c0689880ca3ce868eb21bc4007
shareefuddeen <140364218+shareefuddeen@users.noreply.github.com>
2025-06-19 10:03:33 -1200

    team

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -39,7 +39,7 @@ const Team = () => {
     revalidateOnFocus:false,
     revalidateOnReconnect:false,
   })
-  console.log(member.image);
+  console.log(team.image);
   
     if (!Array.isArray(team)) return <p>No team data found.</p>
 
